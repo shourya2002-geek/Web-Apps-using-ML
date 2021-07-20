e7fb4e317d1ba22c0568fd9b0192631f4534e23a
shourya2002-geek <13.shourya@gmail.com>
2021-07-21 03:47:45 +0530

    Initial Commit

---
+++ index.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import main,NER,translate,token,listen
+
+app = MultiApp()   
+
+app.add_app("Upload", main.app)
+app.add_app("Translate", translate.app)
+app.add_app("NER", NER.app)
+app.add_app("Tokenization and Lemmatization", token.app)
+
+
+
+
+app.run() 
